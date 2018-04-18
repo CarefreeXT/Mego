575fd7d2bb666d5aaa39bcaec06170db9b0bb450
CarefreeXT <CarefreeXT@hotmail.com>
2018-04-18 23:22:56 +0800

    Add Relation Action.

---
+++ src/Caredev.Mego/DataAnnotations/RelationActionAttribute.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) CarefreeXT and Caredev Studios. All rights reserved.
+// Licensed under the GNU Lesser General Public License v3.0.
+// See License.txt in the project root for license information.
+namespace Caredev.Mego.DataAnnotations
+{
+    using Caredev.Mego.Resolve;
+    using System;
+    /// <summary>
+    /// 数据关系操作行为特性。
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class RelationActionAttribute : Attribute
+    {
+        /// <summary>
+        /// 创建行为特性。
+        /// </summary>
+        /// <param name="update">更新时行为。</param>
+        /// <param name="delete">删除时行为。</param>
+        public RelationActionAttribute(EReferenceAction? update, EReferenceAction? delete)
+        {
+            Update = update;
+            Delete = delete;
+        }
+        /// <summary>
+        /// 更新时行为。
+        /// </summary>
+        public EReferenceAction? Update { get; }
+        /// <summary>
+        /// 删除时行为。
+        /// </summary>
+        public EReferenceAction? Delete { get; }
+    }
+}
---
+++ src/Caredev.Mego/Resolve/EReferenceAction.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) CarefreeXT and Caredev Studios. All rights reserved.
+// Licensed under the GNU Lesser General Public License v3.0.
+// See License.txt in the project root for license information.
+namespace Caredev.Mego.Resolve
+{
+    /// <summary>
+    /// 数据关系的引用行为。
+    /// </summary>
+    public enum EReferenceAction
+    {
+        /// <summary>
+        /// 级联删除。
+        /// </summary>
+        Cascade,
+        /// <summary>
+        /// 设置为空。
+        /// </summary>
+        SetNull,
+        /// <summary>
+        /// 设置为默认值。
+        /// </summary>
+        SetDefault,
+        /// <summary>
+        /// 不执行任何操作。
+        /// </summary>
+        NoAction,
+    }
+}
